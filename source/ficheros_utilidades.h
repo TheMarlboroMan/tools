5334489fa328a2d601c9b2259173c536cfd58fbf
TheMarlboroMan <marlborometal@gmail.com>
2016-01-31 13:26:56 +0100

    Crea ficheros_utilidades.

---
+++ source/ficheros_utilidades.h
@@ -0,0 +1,15 @@
+#ifndef FICHEROS_UTILIDADES_H
+#define FICHEROS_UTILIDADES_H
+
+#include <string>
+#include <fstream>
+
+namespace Herramientas_proyecto
+{
+
+bool		existe_fichero(const std::string&);
+std::string	volcar_fichero(const std::string&);
+
+}
+
+#endif
