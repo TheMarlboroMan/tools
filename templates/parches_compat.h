3238ef12ba6d0003da9fa0119b583c48cc333846
TheMarlboroMan <marlborometal@gmail.com>
2016-04-20 15:11:14 +0200

    Fixed non closing namespace

--- templates/parches_compat.h
+++ templates/parches_compat.h
@@ -12,13 +12,15 @@ template <typename T> std::string to_string(const T& n)
 	std::ostringstream stm;
 	stm<<n;
 	return stm.str();
-};
+}
 #endif
 #ifndef WINCOMPIL
 template <typename T> std::string to_string(const T& n)
 {
 	return std::to_string(n);
-};
+}
 #endif
+
+}
 
 #endif
