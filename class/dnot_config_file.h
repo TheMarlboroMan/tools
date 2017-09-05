341a6f52e930f1ae0f99c01bf4fd91d46eab1032
TheMarlboroMan <marlborometal@gmail.com>
2017-09-06 00:03:36 +0200

    Made token_from_path public in config files

--- class/dnot_config_file.h
+++ class/dnot_config_file.h
@@ -15,9 +15,11 @@ class dnot_config_file
 	public:
 
 	//Multipurpose interface.
-	int int_from_path(const std::string& ppath) const {return token_from_path(ppath).get_int();}
-	bool bool_from_path(const std::string& ppath) const {return token_from_path(ppath).get_bool();}
-	std::string string_from_path(const std::string& ppath) const {return token_from_path(ppath).get_string();}
+	int 				int_from_path(const std::string& ppath) const {return token_from_path(ppath).get_int();}
+	bool 				bool_from_path(const std::string& ppath) const {return token_from_path(ppath).get_bool();}
+	std::string 			string_from_path(const std::string& ppath) const {return token_from_path(ppath).get_string();}
+	const tools::dnot_token&	token_from_path(const std::string& c) const;
+	tools::dnot_token&		token_from_path(const std::string& c);
 
 	void reload();
 	void load(const std::string&);
@@ -34,9 +36,6 @@ class dnot_config_file
 		auto& tok=token_from_path(k);
 		tok.set(v);
 	}
-
-	const tools::dnot_token&			token_from_path(const std::string& c) const;
-	tools::dnot_token&				token_from_path(const std::string& c);
 
 	private:
 
