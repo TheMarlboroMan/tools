d30498f8bde00720fdb4feda8f6555f7e0d0c12f
TheMarlboroMan <marlborometal@gmail.com>
2017-09-06 14:23:21 +0200

    Made set public in the dnot config file class

--- class/dnot_config_file.h
+++ class/dnot_config_file.h
@@ -20,22 +20,17 @@ class dnot_config_file
 	std::string 			string_from_path(const std::string& ppath) const {return token_from_path(ppath).get_string();}
 	const tools::dnot_token&	token_from_path(const std::string& c) const;
 	tools::dnot_token&		token_from_path(const std::string& c);
+	template <typename T>	void 	set(const std::string& k, T v)
+	{
+		auto& tok=token_from_path(k);
+		tok.set(v);
+	}
 
 	void reload();
 	void load(const std::string&);
 	void save();
 
 	dnot_config_file(const std::string&);
-
-	protected: 
-
-	//Conversor to string...
-	template <typename T>
-	void 						set(const std::string& k, T v)
-	{
-		auto& tok=token_from_path(k);
-		tok.set(v);
-	}
 
 	private:
 
