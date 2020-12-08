0e457f2e88028dbe131a934802e5310fbb0843c0
The Marlboro Man <marlborometal@gmail.com>
2020-12-08 18:05:23 +0100

    Bug fixing

--- include/tools/grid_list.h
+++ include/tools/grid_list.h
@@ -12,19 +12,27 @@ class grid_list:
 
 	//!Each item of the list: encapsulates position, index and represented value.
 	struct Item {
-		size_t 		x, 	//!< X position.
+		std::size_t 		x, 	//!< X position.
 				y, 	//!< Y position.
 				index;	//!< Zero-based index.
 		const T& item;		//!< Represented value.
 	};
 
 		//!Class constructor. Takes the available size and item size.
-						grid_list(size_t available_w, size_t available_h, size_t item_w, size_t item_h)
-		:available_w(available_w), available_h(available_h),
-		item_w(item_w), item_h(item_h),  
-		margin_w(0), margin_h(0),
-		reg_row(floor(available_w / item_w)),
-		reg_column(floor(available_h / item_h)) {
+						grid_list(
+			std::size_t available_w, 
+			std::size_t available_h, 
+			std::size_t item_w, 
+			std::size_t item_h
+		)
+			:available_w(available_w), 
+			available_h(available_h),
+			item_w(item_w), 
+			item_h(item_h),
+			margin_w(0), 
+			margin_h(0),
+			reg_row{floor(available_w / (item_w+margin_w) )},
+			reg_column{floor(available_h / (item_h+margin_h))} {
 		this->pager_instance.set_items_per_page(reg_row * reg_column);
 	}
 
@@ -47,11 +55,11 @@ class grid_list:
 	//!Seems that it returns the currently selected item. TODO: Where is this used? What does it do?.
 	const Item				current_line() const {
 
-		size_t	index=this->pager_instance.get_current_index(),
+		std::size_t	index=this->pager_instance.get_current_index(),
 			rpp=this->pager_instance.get_items_per_page(),
 			rel=index % rpp;
 
-		size_t	y=floor(rel / reg_row),
+		std::size_t	y=floor(rel / reg_row),
 			x=floor(rel % reg_row);
 
 		return Item{ (x * item_w) + (x * margin_w), (y * item_h) + (y * margin_h), index, this->get()};
@@ -63,16 +71,17 @@ class grid_list:
 	std::vector<Item>			get_page() const {
 		std::vector<Item> res;
 
-		size_t rpp=this->pager_instance.get_items_per_page(),
+		std::size_t rpp=this->pager_instance.get_items_per_page(),
 			pa=this->pager_instance.get_current_page();
 
 		auto	ini=std::begin(this->data)+(pa * rpp),
 			fin=ini+rpp;
 
-		size_t x=0, y=0, reg=1, index=pa * rpp;
+		std::size_t x=0, y=0, reg=1, index=pa * rpp;
 
 		while(ini < fin && ini < std::end(this->data)) {
-			res.push_back(Item{x, y, index++, *ini});
+
+			res.push_back({x, y, index++, *ini});
 			++ini;
 
 			if(reg == reg_row) {
@@ -92,7 +101,11 @@ class grid_list:
 	//!Applies f to the element in the given position. Returns true if there
 	//!was an element in the position, false if not.
 	template<typename E>
-	bool 					topological_select(size_t rx, size_t ry, E f) {
+	bool 					topological_select(
+		std::size_t rx, 
+		std::size_t ry, 
+		E f
+	) {
 		const auto& pag=get_page();
 		for(const auto& itemp : pag) {
 			if(	rx >= itemp.x
@@ -106,26 +119,27 @@ class grid_list:
 		return false;
 	}	
 
-	//!Seems to return true if the current page contains the item v. TODO: I really don't know what this does.
+	//!Seems to return true if the current page contains the item v. 
+	//TODO: I really don't know what this does.
 	bool					set_row(int v) {
 		bool result=false;
-		for(size_t i=0; i < reg_row ; ++i) {
+		for(std::size_t i=0; i < reg_row ; ++i) {
 			result=this->pager_instance.set_item(v) || result;
 		}
 		return result;
 	}
 
 	//!Returns the fixed width for each item.
-	size_t					get_item_w() const {return item_w;}
+	std::size_t					get_item_w() const {return item_w;}
 
 	//!Returns the fixed height for each item.
-	size_t					get_item_h() const {return item_h;}
+	std::size_t					get_item_h() const {return item_h;}
 
 	//!Returns the horizontal margin between items.
-	size_t					get_margin_w() const {return margin_w;}
+	std::size_t					get_margin_w() const {return margin_w;}
 
 	//!Returns the vertical margin between items.
-	size_t					get_margin_h() const {return margin_h;}
+	std::size_t					get_margin_h() const {return margin_h;}
 
 	//!Returns the available width.
 	std::size_t				get_available_w() const {return available_w;}
@@ -134,14 +148,22 @@ class grid_list:
 	std::size_t				get_available_h() const {return available_h;}
 
 	//!Sets the horizontal margin between items.
-	void					set_margin_w(size_t v) {margin_w=v;}
+	void					set_margin_w(std::size_t v) {
+
+		margin_w=v;
+		reg_row=floor(available_w / (item_w+margin_w));
+	}
 
 	//!Sets the vertical margin between items.
-	void					set_margin_h(size_t v) {margin_h=v;}
+	void					set_margin_h(std::size_t v) {
+
+		margin_h=v;
+		reg_column=floor(available_h / (item_h+margin_h));
+	}
 
 	private:
 
-	size_t					available_w,	//!< Available width the for list.
+	std::size_t				available_w,	//!< Available width the for list.
 						available_h,	//!< Available height the for list.
 						item_w, 	//!< Fixed width for each item.
 						item_h,		//!< Fixed height for each item.
