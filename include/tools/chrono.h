3a068ebce8fb356b4c62f38f8a52f34afe5604e8
Daniel Pastor <dpastor@comvive.es>
2021-01-25 13:59:04 +0100

    pause and resume in chrono

--- include/tools/chrono.h
+++ include/tools/chrono.h
@@ -16,7 +16,7 @@ struct chrono_data {
 
 //!Simple chronometer.
 
-//!Returns the time elapsed since "start" was called as an integer. 
+//!Returns the time elapsed since "start" was called as an integer.
 //!There is not need to reset it: after stop, each call to start is valid.
 //!In this case, time is "real" or "user" time.
 
@@ -24,30 +24,43 @@ struct chrono_data {
 class chrono {
 
 	public:
-	
+
 	//!Starts the count.
-	void			start();
+	void            start();
 
 	//!Stops the counter.
-	void			stop();
+	void            stop();
+
+	//!Pauses the counter.
+	void            pause();
+
+	//!Resumes the counter.
+	void            resume();
 
 	//!Returns the total of ms elapsed.
-	int			get_milliseconds();
+	int             get_milliseconds();
 
 	//!Returns the total of seconds elapsed.
-	int			get_seconds();
+	int             get_seconds();
 
 	//!Gets a full structure of hours, minutes, seconds and milliseconds.
-	chrono_data		get_full();
+	chrono_data     get_full();
 
 	//!Returns true if started.
-	bool			is_running() const {return running;}
+	bool            is_running() const {return running;}
+
+	//!Returns true if paused.
+	bool            is_paused() const {return paused;}
 
 	private:
 
-	std::chrono::high_resolution_clock::time_point 	begin,		//!< Internal starting time point.
-							end;		//!< Internal end time point.
-	bool						running=false;	//!< State flag.
+	using tp=std::chrono::high_resolution_clock::time_point;
+
+	tp              begin,		//!< Internal starting time point.
+	                end,		//!< Internal end time point.
+	                pause_point;      //!< Internal pause moment.
+	bool            running=false,
+	                paused;	//!< State flag.
 };
 
 }
--- lib/tools/chrono.cpp
+++ lib/tools/chrono.cpp
@@ -3,26 +3,72 @@
 using namespace tools;
 
 void chrono::start() {
+
 	begin=std::chrono::high_resolution_clock::now();
 	running=true;
 }
 
 void chrono::stop() {
+
 	end=std::chrono::high_resolution_clock::now();
 	running=false;
 }
 
+void chrono::pause() {
+
+	if(paused) {
+
+		return;
+	}
+
+	paused=true;
+	pause_point=std::chrono::high_resolution_clock::now();
+}
+
+void chrono::resume() {
+
+	if(!paused) {
+
+		return;
+	}
+
+	paused=false;
+}
+
 int chrono::get_milliseconds() {
-	if(running) return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
-	else return std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+
+	if(running) {
+
+		if(paused) {
+			return std::chrono::duration_cast<std::chrono::milliseconds>(pause_point - begin).count();
+		}
+		else {
+
+			return std::chrono::duration_cast<std::chrono::milliseconds>(std::chrono::high_resolution_clock::now() - begin).count();
+		}
+	}
+	else {
+		return std::chrono::duration_cast<std::chrono::milliseconds>(end - begin).count();
+	}
 }
 
 int chrono::get_seconds() {
-	if(running) return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - begin).count();
-	else return std::chrono::duration_cast<std::chrono::seconds>(end - begin).count();
+	if(running) {
+
+		if(paused) {
+			return std::chrono::duration_cast<std::chrono::milliseconds>(pause_point - begin).count();
+		}
+		else {
+			return std::chrono::duration_cast<std::chrono::seconds>(std::chrono::high_resolution_clock::now() - begin).count();
+		}
+	}
+	else {
+		return std::chrono::duration_cast<std::chrono::seconds>(end - begin).count();
+	}
 }
 
 chrono_data chrono::get_full() {
+
 	auto t=get_milliseconds();
 
 	//Hyper lazy.
@@ -30,6 +76,6 @@ chrono_data chrono::get_full() {
 	int seconds=(ms/1000) % 60;
 	int minutes=((ms/1000) % 60) % 60;
 	int hours=(((ms/1000) % 60) % 60) % 60;
-	
+
 	return {hours, minutes, seconds, ms};
 }
